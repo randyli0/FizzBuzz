997e26efa6bf7176526ceab4382603ab690d827d
randy li <mirandali705@gmail.com>
2024-09-09 16:36:26 -0400

    created a second java class called FizzBuzz2.java, in this class we rewrote FizzBuzz using a while loop. We also refactored the code.

---
+++ src/FizzBuzz2.java
@@ -0,0 +1,29 @@
+
+
+public class FizzBuzz2 {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 100){
+
+            doFizzBuzz(count);
+            count++;
+        }
+
+
+    }
+
+    private static void doFizzBuzz(int count) {
+        boolean divisibleby3 = count % 3 == 0;
+        boolean divisibleby5 = count % 5 == 0;
+
+        if (divisibleby3 && divisibleby5){
+            System.out.println("fizz Buzz");
+        } else if (divisibleby3){
+            System.out.println("Fizz");
+        } else if (divisibleby5){
+            System.out.println("Buzz");
+        } else{
+            System.out.println(count);
+        }
+    }
+}
